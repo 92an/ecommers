4c005b03456df012465217ab9ccada70b690329b
92an <60664824+92an@users.noreply.github.com>
2021-09-11 16:37:39 +0200

    test

--- ecomsite/ecomsite/urls.py
+++ ecomsite/ecomsite/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from shop.views import detail
 from django.contrib import admin
 from django.urls import path
 from shop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('<int:id>/', views.detail, name="detail"),
 ]
